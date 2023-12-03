c95409f43f7ef37ceac53898271db427afcb5274
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2023-12-03 21:23:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// ANIMATION LIBRARIES USED IN THIS PROJECT :
+// locomotive .js for smooth scroll
+// gsap to animate & Shery.js
+
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
+
